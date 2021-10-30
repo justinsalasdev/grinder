3c7cd7cde01d81b164c44d02f76af4ec65325af5
justinsalasdev <justinsalasdev@gmail.com>
2021-10-30 23:05:29 +0800

    init

---
+++ src/array/two-number-sum/solution1.ts
@@ -0,0 +1,14 @@
+//O(n^2)T & O(1) space
+function twoNumberSum(input: number[], target: number) {
+  for (let i = 0; i < input.length; i++) {
+    const firstNum = input[i];
+    for (let j = i + 1; j < input.length; j++) {
+      const secondNum = input[j];
+      if (firstNum + secondNum === target) {
+        return [firstNum, secondNum];
+      }
+    }
+  }
+  //return if no pair found
+  return [];
+}
---
+++ src/array/two-number-sum/test.ts
@@ -0,0 +1,8 @@
+import * as chai from "chai";
+
+it("Test Case #1", function () {
+  const output = twoNumberSum([3, 5, -4, 8, 11, 1, -1, 6], 10);
+  chai.expect(output.length === 2).to.deep.equal(true);
+  chai.expect(output.includes(11)).to.deep.equal(true);
+  chai.expect(output.includes(-1)).to.deep.equal(true);
+});
